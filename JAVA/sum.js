f4385b925e88ea7b58efd481761e4bc663bccfb7
Adarsh Soman <22mca03@mgits.ac.in>
2022-12-07 16:02:44 +0530

    next commit5

---
+++ JAVA/sum.js
@@ -0,0 +1,6 @@
+function sum() {
+  var n1=document.getElementById("f").value;
+  var n2=document.getElementById("s").value;
+  var s=parseInt(n1)+parseInt(n2);
+  document.getElementById("p").innerHTML=s;
+}
